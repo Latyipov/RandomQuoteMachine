eada2aa9e7842b2c71393e6754b49be9ac2e70d3
Ildar <ildar.latipov@gmail.com>
2022-12-05 21:47:34 +0100

    added html and styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import "./styles/styles.scss";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "@/App/App.js";
-
+import "./styles/styles.scss";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
